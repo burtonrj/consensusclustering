bbdf2de05403329f856f89a1e7e4f977b079c037
burtonrj <burtonrossj@gmail.com>
2023-09-22 21:43:20 +0100

    Added tests - all passing

--- consensusclustering/consensus.py
+++ consensusclustering/consensus.py
@@ -1,35 +1,21 @@
 from __future__ import annotations
 
 from itertools import combinations
-from typing import Protocol
+from typing import Type
 
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
 
-class ClusteringAlgo(Protocol):
-    def fit(self, x: np.ndarray, y=None):
-        ...
-
-    def predict(self, x: np.ndarray) -> np.ndarray:
-        ...
-
-    def fit_predict(self, x: np.ndarray, y=None) -> np.ndarray:
-        ...
-
-    def set_params(self, n_clusters: int, **params):
-        ...
-
-
-def _resample(x: np.ndarray, frac: float) -> tuple[np.ndarray, np.ndarray]:
+def resample(x: np.ndarray, frac: float) -> tuple[np.ndarray, np.ndarray]:
     resampled_indices = np.random.choice(
         range(x.shape[0]), size=int(x.shape[0] * frac), replace=False
     )
     return resampled_indices, x[resampled_indices, :]
 
 
-def _compute_connectivity_matrix(labels: np.ndarray) -> np.ndarray:
+def compute_connectivity_matrix(labels: np.ndarray) -> np.ndarray:
     out_of_bag_idx = np.where(labels == -1)[0]
     connectivity_matrix = np.equal.outer(labels, labels).astype("int8")
     rows, cols = zip(*list(combinations(out_of_bag_idx, 2)))
@@ -39,9 +25,7 @@ def _compute_connectivity_matrix(labels: np.ndarray) -> np.ndarray:
     return connectivity_matrix
 
 
-def _compute_identity_matrix(
-    x: np.ndarray, resampled_indices: np.ndarray
-) -> np.ndarray:
+def compute_identity_matrix(x: np.ndarray, resampled_indices: np.ndarray) -> np.ndarray:
     n = x.shape[0]
     identity_matrix = np.zeros((n, n), dtype="int8")
     rows, cols = zip(*list(combinations(resampled_indices, 2)))
@@ -51,17 +35,29 @@ def _compute_identity_matrix(
     return identity_matrix
 
 
-def _compute_consensus_matrix(
+def compute_consensus_matrix(
     connectivity_matrices: list[np.ndarray], identity_matrices: list[np.ndarray]
 ) -> np.ndarray:
-    return np.sum(connectivity_matrices, axis=0) / np.sum(identity_matrices, axis=0)
+    return np.nan_to_num(
+        np.sum(connectivity_matrices, axis=0) / np.sum(identity_matrices, axis=0), nan=0
+    )
 
 
-def _cluster(
-    x: np.ndarray, resample_frac: float, k: int, clustering_obj: ClusteringAlgo
-) -> tuple[ClusteringAlgo, np.ndarray, np.ndarray]:
+def valid_clustering_obj(clustering_obj) -> bool:
+    if not hasattr(clustering_obj, "fit_predict"):
+        return False
+    if not hasattr(clustering_obj, "set_params"):
+        return False
+    return True
+
+
+def cluster(
+    x: np.ndarray, resample_frac: float, k: int, clustering_obj
+) -> tuple[Type, np.ndarray, np.ndarray]:
+    if not valid_clustering_obj(clustering_obj):
+        raise ValueError("clustering_obj must have fit_predict and set_params methods")
     clustering_obj.set_params(n_clusters=k)
-    resampled_indices, resampled_x = _resample(x, resample_frac)
+    resampled_indices, resampled_x = resample(x, resample_frac)
     resampled_labels = clustering_obj.fit_predict(resampled_x)
     labels = np.full(x.shape[0], -1)
     labels[resampled_indices] = resampled_labels
@@ -71,7 +67,7 @@ def _cluster(
 class ConsensusClustering:
     def __init__(
         self,
-        clustering_obj: ClusteringAlgo,
+        clustering_obj,
         min_clusters: int,
         max_clusters: int,
         n_resamples: int,
@@ -94,13 +90,13 @@ class ConsensusClustering:
             connectivity_matrices = []
             identity_matrices = []
             for _ in range(self.n_resamples):
-                clustering_obj, resampled_indices, labels = _cluster(
+                clustering_obj, resampled_indices, labels = cluster(
                     x, self.resample_frac, k, self.clustering_obj
                 )
-                connectivity_matrices.append(_compute_connectivity_matrix(labels))
-                identity_matrices.append(_compute_identity_matrix(x, resampled_indices))
+                connectivity_matrices.append(compute_connectivity_matrix(labels))
+                identity_matrices.append(compute_identity_matrix(x, resampled_indices))
             self.consensus_matrices_.append(
-                _compute_consensus_matrix(connectivity_matrices, identity_matrices)
+                compute_consensus_matrix(connectivity_matrices, identity_matrices)
             )
 
     def hist(self, k: int):
@@ -118,13 +114,16 @@ class ConsensusClustering:
         ecdf, hist, bins = self.cdf(k)
         return np.sum(ecdf * (bins[1:] - bins[:-1]))
 
-    def best_k(self):
+    def change_in_area_under_cdf(self):
         auc = [
             self.area_under_cdf(k)
             for k in range(self.min_clusters, self.max_clusters + 1)
         ]
-        # Change in area under cdf
-        largest_change_in_auc = np.argmax(np.diff(auc)) + 1
+        return np.diff(auc)
+
+    def best_k(self):
+        change = self.change_in_area_under_cdf()
+        largest_change_in_auc = np.argmax(change) + 1
         return list(range(self.min_clusters, self.max_clusters + 1))[
             largest_change_in_auc
         ]
@@ -132,19 +131,36 @@ class ConsensusClustering:
     def plot_clustermap(self, k: int, **kwargs):
         return sns.clustermap(self.consensus_k(k), **kwargs)
 
+    def plot_hist(self, k: int, ax: plt.Axes | None = None):
+        ax = ax if ax is not None else plt.subplots(figsize=(6.5, 6.5))[1]
+        hist, bins = self.hist(k)
+        ax.bar(bins[:-1], hist, width=np.diff(bins))
+        ax.set_xlabel("Consensus index value")
+        ax.set_ylabel("Density")
+        return ax
+
     def plot_cdf(self, ax: plt.Axes | None = None):
         ax = ax if ax is not None else plt.subplots(figsize=(6.5, 6.5))[1]
         for k in range(self.min_clusters, self.max_clusters + 1):
             ecdf, hist, bins = self.cdf(k)
             ax.plot(bins[:-1], ecdf, label=f"{k} clusters")
+        ax.set_xlabel("Consensus index value")
+        ax.set_ylabel("CDF")
         ax.legend()
         return ax
 
     def plot_change_area_under_cdf(self, ax: plt.Axes | None = None):
         ax = ax if ax is not None else plt.subplots(figsize=(6.5, 6.5))[1]
-        k = [k for k in range(self.min_clusters, self.max_clusters + 1)]
-        auc = [self.area_under_cdf(k) for k in k]
+        k = [k for k in range(self.min_clusters + 1, self.max_clusters + 1)]
+        change = self.change_in_area_under_cdf()
         ax.plot(
-            k, auc, "--", marker="o", markerfacecolor="white", markeredgecolor="black"
+            k,
+            change,
+            "--",
+            marker="o",
+            markerfacecolor="white",
+            markeredgecolor="black",
         )
+        ax.set_xlabel("K")
+        ax.set_ylabel("Change in area under CDF")
         return ax
---
+++ tests/test_consensus.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pytest
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.datasets import make_classification
+
+from consensusclustering.consensus import (
+    ConsensusClustering,
+    compute_connectivity_matrix,
+    compute_consensus_matrix,
+    compute_identity_matrix,
+    resample,
+)
+
+np.random.seed(42)
+
+
+UNIFORM = np.random.uniform(low=0.0, high=1.0, size=(1000, 600))
+GAUSSIAN = make_classification(
+    n_samples=1000,
+    n_features=600,
+    n_informative=600,
+    n_redundant=0,
+    n_repeated=0,
+    n_classes=3,
+    n_clusters_per_class=1,
+    random_state=42,
+    class_sep=10.0,
+)[0]
+LABELS = np.array([0, 1, 1, 2, 0, -1, -1, 2, 2, 1, 3, 3, -1, -1])
+
+
+@pytest.mark.parametrize("frac", [0.1, 0.5, 0.9])
+def test_resample(frac):
+    idx, sample = resample(UNIFORM, frac=frac)
+    assert idx.shape[0] == sample.shape[0]
+    assert idx.shape[0] == int(UNIFORM.shape[0] * frac)
+
+
+def test_connectivity_matrix():
+    connectivity_matrix = compute_connectivity_matrix(LABELS)
+    assert connectivity_matrix.sum() == 26
+    for pairs in [(0, 4), (1, 2), (1, 9), (2, 9), (3, 7), (3, 8), (7, 8), (10, 11)]:
+        if connectivity_matrix[pairs[0], pairs[1]] == 0:
+            print(pairs)
+        assert connectivity_matrix[pairs[0], pairs[1]] == 1
+        assert connectivity_matrix[pairs[1], pairs[0]] == 1
+    for pairs in [(5, 6), (5, 12), (6, 12), (11, 12)]:
+        assert connectivity_matrix[pairs[0], pairs[1]] == 0
+        assert connectivity_matrix[pairs[1], pairs[0]] == 0
+
+
+def test_compute_identity_matrix():
+    identity_matrix = compute_identity_matrix(UNIFORM, np.array([0, 1, 2, 3, 4]))
+    assert identity_matrix.shape == (1000, 1000)
+    assert identity_matrix.sum() == 25
+    for pairs in [
+        (0, 1),
+        (0, 2),
+        (0, 3),
+        (0, 4),
+        (1, 2),
+        (1, 3),
+        (1, 4),
+        (2, 3),
+        (2, 4),
+        (3, 4),
+    ]:
+        assert identity_matrix[pairs[0], pairs[1]] == 1
+        assert identity_matrix[pairs[1], pairs[0]] == 1
+
+
+def test_computer_consensus_matrix():
+    connectivity_matrices = [np.array([[1, 1], [1, 0]]), np.array([[0, 1], [0, 0]])]
+    identity_matrices = [
+        np.array([[1, 1], [1, 1]]),
+        np.array([[1, 1], [0, 0]]),
+    ]
+    consensus_matrix = compute_consensus_matrix(
+        connectivity_matrices, identity_matrices
+    )
+    assert consensus_matrix.shape == (2, 2)
+    assert np.array_equal(consensus_matrix, np.array([[0.5, 1.0], [1.0, 0.0]]))
+
+
+def test_consensus_cluster_uniform():
+    clustering = ConsensusClustering(
+        clustering_obj=AgglomerativeClustering(affinity="euclidean", linkage="ward"),
+        min_clusters=2,
+        max_clusters=6,
+        n_resamples=10,
+        resample_frac=0.8,
+    )
+    clustering.fit(UNIFORM)
+    assert len(clustering.consensus_matrices_) == 5
+    hist, bins = clustering.hist(3)
+    assert np.mean(hist) > 0.7
+    assert clustering.best_k() == 3
+
+
+def test_consensus_cluster_gaussian():
+    clustering = ConsensusClustering(
+        clustering_obj=AgglomerativeClustering(affinity="euclidean", linkage="ward"),
+        min_clusters=2,
+        max_clusters=6,
+        n_resamples=10,
+        resample_frac=0.8,
+    )
+    clustering.fit(GAUSSIAN)
+    assert len(clustering.consensus_matrices_) == 5
+    hist, bins = clustering.hist(3)
+    assert hist[0] > 0.0
+    assert hist[9] > 0.0
+    assert all(hist[1:9] == 0.0)
+    assert clustering.best_k() == 3
